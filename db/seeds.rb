a1ef04de1ab6d13246430756b368a527eb136881
Julien Bouland <bouland.julien@gmail.com>
2020-11-23 16:28:16 +0100

    Seeds added + Links fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Julien = Task.new(title: "Julien", details: "Promener le chien")
+Julien.save!
