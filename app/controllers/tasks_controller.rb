785aea387dc32f8b33e912aca8e5cd5b9c2f3678
Julien Bouland <bouland.julien@gmail.com>
2020-11-24 18:40:14 +0100

    finds tasks by id put in private/before action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,16 @@
 class TasksController < ApplicationController
+  before_action :set_tasks only: [:show]
   def index
     @tasks = Task.all
   end
 
   def show
+  end
+
+  private
+
+  def set_tasks
     @task = Task.find(params[:id])
   end
+
 end
