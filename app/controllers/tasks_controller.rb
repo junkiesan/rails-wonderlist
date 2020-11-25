45d0677d846e53bd0f55a45dac20a85350fd1861
Julien Bouland <bouland.julien@gmail.com>
2020-11-25 20:21:51 +0100

    New/create tasks features added to project

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,10 +7,22 @@ class TasksController < ApplicationController
   def show
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save
+    redirect_to tasks_path
+  end
   private
 
   def set_tasks
     @task = Task.find(params[:id])
   end
 
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
   get 'tasks/:id', to: 'tasks#show', as: :task
   root to: 'pages#home'
 end
